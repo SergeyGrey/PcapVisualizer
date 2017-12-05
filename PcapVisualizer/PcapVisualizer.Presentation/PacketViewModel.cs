460d343bc2363fa796031c619bb332de36c1d8b3
SergeyGrey <sergeykorovin285@gmail.com>
2017-12-06 01:25:43 +0300

    Закончил TCP Parser, написал к нему тесты

--- PcapVisualizer/PcapVisualizer.Presentation/PacketViewModel.cs
+++ PcapVisualizer/PcapVisualizer.Presentation/PacketViewModel.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PcapVisualizer.Model;
+﻿using PcapVisualizer.Model;
+using PcapVisualizer.Model.Packets;
 
 namespace PcapVisualizer.Presentation
 {
-    public class PacketViewModel : MyPacket
+    public class PacketViewModel : Packet
     {
     }
 }
